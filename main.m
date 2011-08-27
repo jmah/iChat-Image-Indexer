4ca7cd65d9164a02927a2f95e7c49c1b5ad52293
Jonathon Mah <me@JonathonMah.com>
2011-08-26 18:20:00 -0700

    Initial creation of empty project

---
+++ main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  iChat Image Indexer
+//
+//  Created by Jonathon Mah on 2011-08-26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+int main(int argc, const char *argv[])
+{
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
